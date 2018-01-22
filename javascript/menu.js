28e603c587044d1efc508553ce9a4755d1a24a33
LukeBennettUK <luke5495@gmail.com>
2018-01-22 23:37:13 +0000

    Finish Hamburger Menu. Styling of Hamburger hover
    adding of background to header, writing, about me, beginning of
    portfolio.

---
+++ javascript/menu.js
@@ -0,0 +1,9 @@
+function openSideMenu() {
+  document.getElementById('side-menu').style.width = '100%';
+  document.getElementById('side-menu').style.marginLeft = '0';
+}
+
+function closeSideMenu() {
+  document.getElementById('side-menu').style.width = '0';
+  document.getElementById('side-menu').style.marginLeft = '0';
+}
