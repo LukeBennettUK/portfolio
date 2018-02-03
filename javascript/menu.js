b6f57336f0ff0f5a37d6f67aed285a82b8640cc2
LukeBennettUK <luke5495@gmail.com>
2018-02-03 18:09:38 +0000

    Completed fixed sidebar menu

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,6 +1,7 @@
 function openSideMenu() {
-  document.getElementById('side-menu').style.width = '100%';
-  document.getElementById('side-menu').style.marginLeft = '0';
+  document.getElementById('side-menu').style.width = '60%';
+  document.getElementById('side-menu').style.maxWidth = '400px';
+  document.getElementById('side-menu').style.marginLeft = 0;
 }
 
 function closeSideMenu() {
